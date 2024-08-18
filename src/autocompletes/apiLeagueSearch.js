c3bc10b6f0fb59931ed0e8bc5b092409288e9de5
Leo <debonnetleo4@gmail.com>
2024-08-18 13:01:45 +0200

    autocompletes

--- src/autocompletes/apiLeagueSearch.js
+++ src/autocompletes/apiLeagueSearch.js
@@ -14,7 +14,7 @@ export const autocomplete = {
             action: `cargoquery`,
             tables: `Leagues=l`,
             fields: `l.League, l.League_Short=Short`,
-            where : `l.League LIKE '${input}' OR l.League_Short LIKE '${input}' AND l.League_Short <> null`,
+            where : `l.League LIKE '${input}' OR l.League_Short LIKE '${input}'`,
             order_by: `l.League_Short`,
             limit: 25
         });
