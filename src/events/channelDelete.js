8a6f0db965ee103fd0ed7c2b6ce4425028b726d5
Leo <debonnetleo4@gmail.com>
2024-08-18 09:59:58 +0200

    delete routine when channel is deleted

---
+++ src/events/channelDelete.js
@@ -0,0 +1,14 @@
+import { Events } from 'discord.js';
+import { db } from '../connections/database.js';
+//import cron from 'node-cron';
+
+// Executed when bot is ready
+export const event = {
+    name: Events.ChannelDelete,
+    async execute(channel){
+        // Bot is ready message
+
+        const sql = `DELETE FROM routine WHERE routine_channel_id = ${channel.id} AND routine_guild_id = ${channel.guild.id}`;
+        await db.query(sql);
+    }
+}
